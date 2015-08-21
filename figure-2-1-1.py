3fe6f70e3c07d2a81462258f962664c33778bbaf
Ryan Dorson <slyryd@gmail.com>
2015-08-21 01:48:06 -0700

    Modified figure 2-1-1.py to match description.

--- figure-2-1-1.py
+++ figure-2-1-1.py
@@ -7,11 +7,11 @@ from numpy import *
 from pylab import *
 
 # Define vectors and scalars
-c = 2
 x = array([1, 2])
 y = array([2, 1])
 z = x + y
-cx = c * x
+half_x = 0.5 * x
+two_x = 2 * x
 
 # Plot vectors
 fig = figure(figsize=(20, 8))
@@ -20,6 +20,18 @@ prop = FontProperties()
 prop.set_file('STIXMath-Regular.otf')
 
 subplot(1, 2, 1)
+quiver((0, 0, 0), (0, 0, 0), (two_x[0], x[0], half_x[0]), (two_x[1], x[1], half_x[1]), angles='xy', scale_units='xy', scale=1, color=('purple', 'r', 'b'))
+labels = [u'2x = \u27E82, 4\u27E9', u'x = \u27E81, 2\u27E9', u'0.5x = \u27E80.5, 1\u27E9']
+xs = [two_x[0], x[0], half_x[0]]
+ys = [two_x[1], x[1], half_x[1]]
+for label, u, v in zip(labels, xs, ys):
+	annotate(label, xy = (u + 0.05, v), fontproperties=prop)
+title('Scalar Product')
+xlim([0, 5])
+ylim([0, 5])
+draw()
+
+subplot(1, 2, 2)
 plot((x[0], z[0]), (x[1], z[1]), '--r')
 plot((y[0], z[0]), (y[1], z[1]), '--b')
 quiver((0, 0, 0), (0, 0, 0), (x[0], y[0], z[0]), (x[1], y[1], z[1]), angles='xy', scale_units='xy', scale=1, color=('b', 'r', 'purple'))
@@ -27,20 +39,8 @@ labels = [u'x = \u27E81, 2\u27E9', u'y = \u27E82, 1\u27E9', u'x + y = \u27E83, 3
 xs = [x[0], y[0], z[0]]
 ys = [x[1], y[1], z[1]]
 for label, u, v in zip(labels, xs, ys):
-	annotate(label, xy = (u, v), fontproperties=prop)
+	annotate(label, xy = (u + 0.05, v), fontproperties=prop)
 title('Vector Addition')
-xlim([0, 5])
-ylim([0, 5])
-draw()
-
-subplot(1, 2, 2)
-quiver((0, 0), (0, 0), (cx[0], x[0]), (cx[1], x[1]), angles='xy', scale_units='xy', scale=1, color=('r', 'b'))
-labels = [u'x = \u27E81, 2\u27E9', u'2x = \u27E82, 4\u27E9']
-xs = [x[0], cx[0]]
-ys = [x[1], cx[1]]
-for label, u, v in zip(labels, xs, ys):
-	annotate(label, xy = (u, v), fontproperties=prop)
-title('Scalar Product')
 xlim([0, 5])
 ylim([0, 5])
 draw()
